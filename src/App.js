ea32f9dd7e1a062b43f1187d1db9dff498dd325d
hoyosdev <hoyosdev@gmail.com>
2024-07-25 18:41:58 -0300

    Add src folder

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './Components/Header'
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+    <div className='page-wrapper'>
+      <Header></Header>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+import illustration from './../images/illustration-working.svg';
+
+
+function Header() {
+    return <>
+            <header className='header'>
+                <div className='container'>
+                <div className='header-content'>
+                    <h1 className='header-title'>More than just shorter links </h1>
+                    <p className='header-subtitle'>Build your brand's recognition and get detailed insights on how your links are performing.</p>
+                    <button className='btn-primary'>Get started</button>
+                </div>
+                </div>
+                <img src={illustration} className='header-img' alt='A person working in a computer' />
+            </header>
+        </>
+}
+
+export default Header;
